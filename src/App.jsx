18178cee02c0c23eba43c89d63b6e3ea97732016
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-02-02 14:10:00 -0300

    docs: project init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-normal underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
