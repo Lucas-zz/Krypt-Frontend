8493a49580ee4bf9f89433d8aa9a5e4a59a1d899
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-02-03 00:18:08 -0300

    feat: finishing project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Navbar, Welcome, Services, Transactions, Footer } from "./components"
+import Toast from "./components/Toaster";
+
 
 const App = () => (
   <div className="min-h-screen">
+    <Toast />
     <div className="gradient-bg-welcome">
       <Navbar />
       <Welcome />
